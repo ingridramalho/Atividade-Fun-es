c62ca7428bd501b2c71ad165459358959ce02d91
ingridramalho <142914251+ingridramalho@users.noreply.github.com>
2024-10-11 08:49:56 -0300

    script.js finalizado

---
+++ script.js
@@ -0,0 +1,11 @@
+function Multiplicar () {
+    const numero1 = Number(document.querySelector("#numero1").value)
+    const numero2 = Number(document.querySelector("#numero2").value)
+    const numero3 = Number(document.querySelector("#numero3").value)
+    
+    const resultado = document.querySelector(".resultado")
+    resultado.innerHTML = Multiplicar: ${numero1*numero2*numero3}
+}
+
+const button = document.querySelector("button")
+button.addEventListener("click", Multiplicar)
